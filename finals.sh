1d716d0155e298d45def05b5ec989ae407ded2c4
Deguzman1996 <deguzman.joshuafer@auf.edu.ph>
2021-10-06 18:37:53 +0800

    add script

---
+++ finals.sh
@@ -0,0 +1,34 @@
+router 1
+
+vi lamp.sh
+
+#!/bin/bash
+
+echo "Installing HTTPD"
+yum install -y httpd
+echo "Starting HTTPD"
+systemctl start httpd.service
+
+echo "Adding Firewall Rules"
+firewall-cmd --add-port 80/tcp --permanent
+firewall-cmd --reload
+
+#cat lamp.sh
+
+#router 2
+
+#vi lamp.sh
+
+echo "Installing HTTPD"
+yum install -y httpd
+echo "Starting HTTPD"
+systemctl start httpd.service
+
+echo "Adding Firewall Rules"
+firewall-cmd --add-port 80/tcp --permanent
+firewall-cmd --reload
+
+#cat lamp.sh
+
+#chmod +x lamp.sh
+#./lamp.sh
