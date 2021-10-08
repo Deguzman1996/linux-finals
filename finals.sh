141164374b2cfd1d71ed1525e55e8e20f51bce66
Deguzman1996 <89532860+Deguzman1996@users.noreply.github.com>
2021-10-08 18:49:56 +0800

    Update finals.sh

--- finals.sh
+++ finals.sh
@@ -13,6 +13,47 @@ echo "Adding Firewall Rules"
 firewall-cmd --add-port 80/tcp --permanent
 firewall-cmd --reload
 
+echo "Enabling HTTPD.SERVICE"
+sudo systemctl enable httpd.service
+
+echo "Stop hhtpd"
+sudo systemctl stop httpd
+
+echo "disable httpd"
+sudo systemctl disable httpd
+
+echo "enable httpd"
+sudo systemctl enable httpd
+
+echo "restart httpd"
+sudo systemctl restart httpd
+
+echo "install php php-mysql"
+sudo yum install php php-mysql
+
+echo " install php php-mysql"
+sudo systemctl install php php-mysql
+
+echo "search php-"
+yum search php-
+
+echo "info lamp"
+yum info lamp.sh
+
+echo "infophp"
+yum info php-fpm
+
+echo "Install php"
+sudo yum install php-fpm
+
+echo "Install package"
+sudo yum install package1 package2 
+
+echo "website"
+sudo vi /var/www/html/info.php
+
+<?php phpinfo(); ?>
+
 #cat lamp.sh
 
 #router 2
